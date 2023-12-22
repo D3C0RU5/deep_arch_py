2d9a464226bbadffc21d17d40065d0ff0d4a23e5
che <che@certi.org.br>
2023-12-22 11:20:42 -0300

    refactor: move server error to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,6 @@
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
-import { ServerError } from '../errors/server-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { type Controller } from '../protocols/controller'
 import { type EmailValidator } from '../protocols/email-validator'
 import { type HttpResponse, type HttpRequest } from '../protocols/http'
@@ -28,7 +27,7 @@ export class SignUpController implements Controller {
 
       return { statusCode: 200, body: 'success' }
     } catch {
-      return { statusCode: 500, body: new ServerError() }
+      return serverError()
     }
   }
 }
