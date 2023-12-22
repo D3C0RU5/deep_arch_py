4ab977503de29dc08414cc54d3694324fdd30991
che <che@certi.org.br>
2023-12-21 23:07:19 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,13 +4,13 @@ import { type HttpResponse, type HttpRequest } from '../protocols/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
 
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
-    }
     return { statusCode: 200, body: 'success' }
   }
 }
