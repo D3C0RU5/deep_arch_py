b3f28350ed3918999597a9e261b7b917047a2b36
che <che@certi.org.br>
2023-12-26 14:48:06 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -15,10 +15,19 @@ describe('EmailValidator Adapter ', () => {
 
     expect(isValid).toBe(false)
   })
-  test('Return false if validator returns truen', () => {
+
+  test('Return false if validator returns true', () => {
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('valid_email@mail.com')
 
     expect(isValid).toBe(true)
+  })
+
+  test('Call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@mail.com')
+
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
