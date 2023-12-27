79c2c6a5640ac2f0e23e4dee88ec42692361225a
che <che@certi.org.br>
2023-12-27 14:41:12 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct values

---
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -0,0 +1,15 @@
+import bcrypt from 'bcrypt'
+import { type Encrypter } from '../../data/protocols/encrypter'
+
+export class BcryptAdapter implements Encrypter {
+  private readonly salt: number
+
+  constructor (salt: number) {
+    this.salt = salt
+  }
+
+  async encrypt (value: string): Promise<string> {
+    const hash = await bcrypt.hash(value, this.salt)
+    return await new Promise(resolve => { resolve(hash) })
+  }
+}
